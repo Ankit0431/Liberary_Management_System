202a903109e5cb6efc92684147e203aae45fc205
Harshita Khare <kharehs@rknec.edu>
2023-12-18 22:26:53 +0530

    Book-review
    Book-Review files in database folder

--- LiberaryManagementSystem/database/migrations/2023_12_18_132129_create_review_table.php
+++ LiberaryManagementSystem/database/migrations/2023_12_18_132129_create_review_table.php
@@ -10,19 +10,21 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-    }
+{
+    Schema::create('reviews', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+        $table->string('message');
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('reviews');
     }
 };
