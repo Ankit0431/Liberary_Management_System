f8775238d81dcad6a82f0c8b81cb0e4f7b4d9d35
Harshita Khare <kharehs@rknec.edu>
2023-12-18 22:42:50 +0530

    Create route in web.php
    Create route in web.php for reviews

--- LiberaryManagementSystem/routes/web.php
+++ LiberaryManagementSystem/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\BookCountController;
+
+
+// routes/web.php
+
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,17 +26,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('reviews', ReviewController::class)
+    ->only(['index', 'store', 'edit', 'update', 'destroy'])
+    ->middleware(['auth', 'verified']);
+    
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('books', BookController::class);
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/reviews', ReviewController::class)->only(['index', 'create', 'store']);
+    Route::get('/borrowed', [BookController::class, 'borrowedBooks'])->name('borrowed.index');
 });
+
+use App\Http\Controllers\BorrowController;
+
+Route::resource('borrows', BorrowController::class)->middleware('auth');
+// routes/web.php
+
+
+
 
 Route::get('books/{id}/manage-count', [BookCountController::class, 'manageCount'])->name('books.manageCount');
 Route::put('books/{id}/update-count', [BookCountController::class, 'updateCount'])->name('books.updateCount');
+
+
+
 
 require __DIR__.'/auth.php';
