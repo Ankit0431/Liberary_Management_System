9a40ef91cf13603dbd825528dd9079f01bcf6f4a
Sakshi Prasad <147254616+sakshiprasad06@users.noreply.github.com>
2023-12-18 22:11:23 +0530

    Update index.blade.php
    copyed index.php

--- LiberaryManagementSystem/resources/views/borrowed/index.blade.php
+++ LiberaryManagementSystem/resources/views/borrowed/index.blade.php
@@ -1,28 +1,46 @@
-<!-- borrowed.blade.php -->
-<!-- borrowed/index.blade.php -->
-@extends('layouts.app')
+<!-- resources/views/dashboard/index.blade.php -->
 
-@section('content')
-    <div class="container mt-4">
-        <h1 class="mt-5 mb-4">Borrowed Books</h1>
+<!-- resources/views/dashboard/index.blade.php -->
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+        </x-slot>
 
-        @forelse ($borrows as $borrow)
-            <div class="card mb-3">
-                <div class="card-body">
-                    <p class="card-text">
-                        <strong>Title:</strong> {{ $borrow->book->title }}
-                        <br>
-                        <strong>Due Date:</strong>
-                        @if ($borrow->due_date < now())
-                            <span class="text-danger">{{ \Carbon\Carbon::parse($borrow->due_date)->format('Y-m-d') }} (Overdue)</span>
-                        @else
-                            {{ \Carbon\Carbon::parse($borrow->due_date)->format('Y-m-d') }}
-                        @endif
-                    </p>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
+                <div class="flex items-center justify-between mb-4">
+                    <h1 class="text-2xl font-bold text-gray-800">Welcome to the Dashboard!</h1>
+                    <!-- Add any other components or buttons here -->
                 </div>
+
+                <!-- Add some additional content with styling -->
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <!-- Card 1 -->
+                    <div class="bg-blue-500 text-white p-4 rounded-md">
+                        <h2 class="text-lg font-semibold mb-2">Card 1</h2>
+                        <p>Your content here.</p>
+                    </div>
+
+                    <!-- Card 2 -->
+                    <div class="bg-green-500 text-white p-4 rounded-md">
+                        <h2 class="text-lg font-semibold mb-2">Card 2</h2>
+                        <p>Your content here.</p>
+                    </div>
+
+                    <!-- Card 3 -->
+                    <div class="bg-orange-500 text-white p-4 rounded-md">
+                        <h2 class="text-lg font-semibold mb-2">Card 3</h2>
+                        <p>Your content here.</p>
+                    </div>
+                </div>
+
+                <!-- Add more sections and components as needed -->
+
             </div>
-        @empty
-            <p>No borrowed books at the moment.</p>
-        @endforelse
+        </div>
     </div>
-@endsection
+    
+</x-app-layout>
